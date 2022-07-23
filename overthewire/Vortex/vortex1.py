e08910f5c099761e1ccb5e78fb7629b970400e3a
DragonTechRoyale <DragonTechRoyale@users.noreply.github.com>
2022-07-23 16:27:44 +0300

    vortex0 finished

--- overthewire/Vortex/vortex1.py
+++ overthewire/Vortex/vortex1.py
@@ -4,7 +4,9 @@ p = process("./vortex0")
 sleep(1)
 tmp = p.recv().split()
 username = tmp[1]
-password = tmp[-1]
+password = tmp[-1].replace('\n', '')
 print("Username: " + username + " Password: " + password)
 
-shell = ssh(host="vortex.labs.overthewire.org ", user=username, password=password, port=22)
+shell = ssh(host="vortex.labs.overthewire.org", user=username, password=password, port=2228)
+shell.interactive()
+p = process("/vortex/vortex1")
